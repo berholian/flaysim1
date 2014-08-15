188da9bcdebb0e9f5b4a1fa373c08661ac410bf7
jose a. Estevez <j.a.esteveza1@gmail.com>
2014-08-16 01:41:20 +0200

    intento subir las 2 branches

--- development/js/main.js
+++ development/js/main.js
@@ -12,11 +12,15 @@ function animar(){
 	var canvas = document.getElementById('stela');
 	var contexto = canvas.getContext("2d");
 
-	/*contexto.clearRect('0,0,2000,24');*/
+	contexto.clearRect('0,0,2000,24');
 	contexto.fillStyle ="rgb(255,255,255)";
 	contexto.fillRect(x,y,18,50);
 
+	/*var img = new image();
+	img.src = "../images/rastro.png";
+	contexto.drawImage(img,x,y);*/
+	
 	x++;
 
-	setTimeout("animar()",50);
+	setTimeout("animar()",1000);
 }
