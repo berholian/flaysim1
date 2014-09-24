715304ca1c22c48d192f51fcac29bfbe0969bcd2
jose a. Estevez <j.a.esteveza1@gmail.com>
2014-09-24 10:43:07 +0200

    cambio h5 por clase en galeria

--- production/desarrollo.php
+++ production/desarrollo.php
@@ -1,6 +1,5 @@
 <!doctype html>
 
-
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
@@ -14,123 +13,121 @@
   <title>desarrollo flysim</title>
 </head>
 
-<body class="desarrollo">
+  <body class="desarrollo">
 
-    <?php include('header.php') ?>
+      <?php include('header.php') ?>
 
-    <section>
-      <h2>desarrollo</h2>
-      <div class="row carta">
-        <?php
-            include('admin/inc/config.php');
-            include('admin/classes/User.php');
-        ?>
+      <section>
+        <h2>desarrollo</h2>
+        <div class="row carta">
+          <?php
+              include('admin/inc/config.php');
+              include('admin/classes/User.php');
+          ?>
 
-        <div class="paper col8">
-          <h4>
-            <a data-destino="#sim_personaliz">simuladores personalizados</a>
-          </h4>
-          <ul id="sim_personaliz">
+          <div class="paper col8">
+            <h4>
+              <a data-destino="#sim_personaliz">simuladores personalizados</a>
+            </h4>
+            <ul id="sim_personaliz">
 
-            <?php
-                $user = new User();
-                $sim_personaliz = $user->getMenu('sim_personaliz');
-                foreach ($sim_personaliz as $curso) {
-                    $template = file_get_contents('templates/menu_item.html');
-                    $template = str_replace('#name#', $curso["name"], $template);
-                    $template = str_replace('#description#', $curso["description"], $template);
-                    echo $template;
-                }
-            ?>
+              <?php
+                  $user = new User();
+                  $sim_personaliz = $user->getMenu('sim_personaliz');
+                  foreach ($sim_personaliz as $curso) {
+                      $template = file_get_contents('templates/menu_item.html');
+                      $template = str_replace('#name#', $curso["name"], $template);
+                      $template = str_replace('#description#', $curso["description"], $template);
+                      echo $template;
+                  }
+              ?>
 
-          </ul>
+            </ul>
+          </div>
+
+          <div class="paper col8">
+            <h4>
+              <a data-destino="#mantenimiento_ampliacion">mantenimiento y ampliación</a>
+            </h4>
+            <ul id="mantenimiento_ampliacion">
+
+              <?php
+                  $user = new User();
+                  $mantenimiento_ampliacion = $user->getMenu('mantenimiento_ampliacion');
+                  foreach ($mantenimiento_ampliacion as $curso) {
+                      $template = file_get_contents('templates/menu_item.html');
+                      $template = str_replace('#name#', $curso["name"], $template);
+                      $template = str_replace('#description#', $curso["description"], $template);
+                      echo $template;
+                  }
+              ?>
+
+            </ul>
+          </div>
+
+          <div class="paper col8">
+            <h4>
+              <a data-destino="#componentes_reales">componentes reales</a>
+            </h4>
+            <ul id="componentes_reales">
+
+              <?php
+                  $user = new User();
+                  $componentes_reales = $user->getMenu('componentes_reales');
+                  foreach ($componentes_reales as $curso) {
+                      $template = file_get_contents('templates/menu_item.html');
+                      $template = str_replace('#name#', $curso["name"], $template);
+                      $template = str_replace('#description#', $curso["description"], $template);
+                      echo $template;
+                  }
+              ?>
+
+            </ul>
+          </div>
         </div>
+      </section>
 
-        <div class="paper col8">
-          <h4>
-            <a data-destino="#mantenimiento_ampliacion">mantenimiento y ampliación</a>
-          </h4>
-          <ul id="mantenimiento_ampliacion">
-
-            <?php
-                $user = new User();
-                $mantenimiento_ampliacion = $user->getMenu('mantenimiento_ampliacion');
-                foreach ($mantenimiento_ampliacion as $curso) {
-                    $template = file_get_contents('templates/menu_item.html');
-                    $template = str_replace('#name#', $curso["name"], $template);
-                    $template = str_replace('#description#', $curso["description"], $template);
-                    echo $template;
-                }
-            ?>
-
-          </ul>
-        </div>
-
-        <div class="paper col8">
-          <h4>
-            <a data-destino="#componentes_reales">componentes reales</a>
-          </h4>
-          <ul id="componentes_reales">
-
-            <?php
-                $user = new User();
-                $componentes_reales = $user->getMenu('componentes_reales');
-                foreach ($componentes_reales as $curso) {
-                    $template = file_get_contents('templates/menu_item.html');
-                    $template = str_replace('#name#', $curso["name"], $template);
-                    $template = str_replace('#description#', $curso["description"], $template);
-                    echo $template;
-                }
-            ?>
-
-          </ul>
+          <!-- flipster-->
+      <div id="Main-Content">
+        <div class="Container">
+          <!-- Flipster List -->
+          <div class="flipster">
+            <div class="galeria">galería</div>
+            <ul class="marco">
+              <li>
+                <img src="images/Sport-1.jpeg">
+              </li>
+              <li>
+                <img src="images/Sport-2.jpg">
+              </li>
+              <li>
+                <img src="images/Sport-3.jpg">
+              </li>
+              <li>
+                <img src="images/Sport-4.jpg">
+              </li>
+              <li>
+                <img src="images/Sport-5.jpg">
+              </li>
+            </ul>
+          </div>
+          <!-- End Flipster List -->
         </div>
       </div>
-    </section>
+          <!-- End flipster-->
 
-    <!-- flipster-->
-    <div id="Main-Content">
-      <div class="Container">
-    <!-- Flipster List -->
-        <div class="flipster">
-        <h4>galería</h4>
-          <ul>
-            <li>
-              <img src="images/Sport-1.jpeg">
-            </li>
-            <li>
-              <img src="images/Sport-2.jpg">
-            </li>
-            <li>
-              <img src="images/Sport-3.jpg">
-            </li>
-            <li>
-              <img src="images/Sport-4.jpg">
-            </li>
-            <li>
-              <img src="images/Sport-5.jpg">
-            </li>
-          </ul>
-        </div>
-    <!-- End Flipster List -->
+      <?php include('footer.php') ?>
 
-      </div>
-    </div>
+      <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+      <script src="js/jquery.flipster.min.js"></script>
+      <script>
 
-    <?php include('footer.php') ?>
-
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-    <script src="js/jquery.flipster.min.js"></script>
-    <script>
-    <!--
-
-      $(function(){ 
-        $(".flipster").flipster({
-          style: 'carousel'
+        $(function(){ 
+          $(".flipster").flipster({
+            style: 'carousel'
+          });
         });
-      });
 
-    -->
-    </script>
-</body>
+      </script>
+  </body>
 </html>
